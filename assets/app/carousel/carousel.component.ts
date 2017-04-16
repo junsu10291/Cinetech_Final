ac9bc08a2a9341faf43d3cf27cfb49434df8b2d2
junsu1029 <junsu_choi@brown.edu>
2017-04-16 17:47:43 -0400

    hi

--- assets/app/carousel/carousel.component.ts
+++ assets/app/carousel/carousel.component.ts
@@ -7,18 +7,39 @@ import { Component, Input } from "@angular/core";
     
     `]
 })
-export class CarouselComponent {        
-    // carouselLeft() {
-    //     $('#main-carousel').carousel('prev');
-    // }
+export class CarouselComponent {   
+    myInterval = 5000;
+    noWrapSlides = false;
+    active = 0;
+    currIndex = 0;
+    slides = [];
 
-    // carouselRight() {
-    //     $('#main-carousel').carousel('next');
-    // }
+    addSlide() {
+        var newWidth = 600 + this.slides.length + 1;
+        this.slides.push({
+            image: '../../public/img/carousel_1.jpg',
+            text: ['Nice image','Awesome photograph','That is so cool','I love that'][this.slides.length % 4],
+            id: this.currIndex++
+        });
+    }
 
-    // modalTrigger() {
-    //     $('.movieDetailModal').modal('toggle');
-    // }
+    assignNewIndexesToSlides(indexes) {
+        for (var i = 0, l = this.slides.length; i < l; i++) {
+            this.slides[i].id = indexes.pop();
+        }
+    }
 
-    constructor() {};
+    constructor() {
+        this.myInterval = 5000;
+        this.noWrapSlides = false;
+        this.active = 0;
+        this.currIndex = 0;
+        this.slides = [];
+
+        for (var i = 0; i < 4; i++) {
+            this.addSlide();
+        }
+
+        console.log(this.slides);
+    }
 }
