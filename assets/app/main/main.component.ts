58a7be4e3bc65a2194c4f474f7ca9d61ce520464
junsu1029 <junsu_choi@brown.edu>
2017-04-17 17:32:35 -0400

    modal

--- assets/app/main/main.component.ts
+++ assets/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { Movies } from '../movies/mock-movie';
 import { Movie } from "../movies/movie.model";
+import { MovieDetailModalComponent } from "../movies/movie-detail-modal.component";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
     selector: 'app-main',
@@ -15,5 +17,15 @@ export class MainComponent implements OnInit {
     ngOnInit() {
         this.movies = Movies;
     }
-    
+
+    constructor(private modalService: NgbModal) {}
+
+    openModal() {
+        this.modalService
+        const modalRef = this.modalService.open(
+            MovieDetailModalComponent, 
+            { size: "lg" });
+
+        //modalRef.componentInstance.name = 'World';
+    }
 }
