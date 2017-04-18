6d6cf47f3bd2aa607da21736d9267ce245b612b1
Jongje Kim <jongje_kim@brown.edu>
2017-04-18 11:52:24 -0400

    personal page updated

--- assets/app/movies/mock-movie.ts
+++ assets/app/movies/mock-movie.ts
@@ -20,6 +20,5 @@ export const Movies: Movie[] = [
     new Movie('20th Century Women', './img/default_poster.jpg'),
     new Movie('20th Century Women', './img/default_poster.jpg'),
     new Movie('20th Century Women', './img/default_poster.jpg'),
-    new Movie('20th Century Women', './img/default_poster.jpg'),
-    new Movie('20th Century Women', './img/default_poster.jpg')    
-];
+    new Movie('20th Century Women', './img/default_poster.jpg')
+    ];
