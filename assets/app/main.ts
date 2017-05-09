9a7877dfd26034fc1ee23191a0e9f4ead4be5bd7
junsu1029 <junsu_choi@brown.edu>
2017-05-09 12:43:32 -0400

    entergit add -A

--- assets/app/main.ts
+++ assets/app/main.ts
@@ -3,5 +3,7 @@ import './polyfills';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from "./app.module";
+import {enableProdMode} from '@angular/core';
 
+enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
